d19d1d35614c853350938bc3a33499b27fac9b8b
Lucas Lentz <lucas.lentz@hotmail.com>
2018-05-05 00:07:32 -0300

    Add exception feature

--- src/Network/Postmon.hs
+++ src/Network/Postmon.hs
@@ -1,4 +1,4 @@
-{-# LANGUAGE OverloadedStrings, DeriveGeneric, RecordWildCards #-}
+{-# LANGUAGE OverloadedStrings, DeriveGeneric, RecordWildCards, ScopedTypeVariables #-}
 
 module Network.Postmon 
     ( fetchPosts
@@ -9,6 +9,7 @@ import Data.Aeson
 import Data.Aeson.Types
 import Data.Text
 import GHC.Generics
+import Control.Exception (try)
 
 gateway :: String -> String
 gateway = (++) "http://api.postmon.com.br/v1/rastreio/ect/"
@@ -38,18 +39,20 @@ history = withObject "history" (.: "historico")
 
 get :: String -> IO (Maybe [History])
 get code = do
-  response <- simpleHttp $ gateway code
-  return $ parseMaybe history =<< decode response
+  response <- try . simpleHttp $ gateway code
+  case response of
+    Left (e :: HttpException) -> return Nothing
+    Right r -> return $ parseMaybe history =<< decode r
 
 statusPost :: History -> String
 statusPost (History {status="Objeto entregue ao destinat\239\191\189rio", date=d, local=l}) =
   "Anta Baka?! Your package has been delivered in " ++ l ++ " since " ++ d ++ "!"
-statusPost (History {status=s, date=d, local=l}) = "How can *I* know?"
+statusPost (History {status=s, date=d, local=l}) = "How can *I* know? :rage:"
 
 fetchPosts :: Text -> IO Text
 fetchPosts code = do 
   content <- get $ unpack code
   case content of
     Just a -> return . pack . statusPost $ Prelude.last a
-    Nothing -> return "Nothing found actually"
+    Nothing -> return "Of course I haven't found anything: It-Doesn't-Even-Exist! :rage:"
   
