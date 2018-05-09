34853b4e255f04882c72f6758be822167ed1cbfa
Lucas Lentz <lucas.lentz@hotmail.com>
2018-05-09 10:42:26 -0300

    Environment config and heroku setup

--- src/Bot.hs
+++ src/Bot.hs
@@ -10,6 +10,7 @@ import Data.Text.Manipulate as L
 import Pipes
 import Data.Monoid
 import Network.Discord
+import System.Environment
 
 reply :: Message -> Text -> Effect DiscordM ()
 reply Message{messageChannel=chan} cont = fetch' $ CreateMessage chan cont Nothing
@@ -21,14 +22,17 @@ replyPost :: (Text -> IO Text)
 replyPost = fetchPosts . L.dropWord 
   
 run :: IO ()
-run = runBot (Bot "TOKEN") $ do
-  with ReadyEvent $ \(Init v u _ _ _) ->
-    liftIO . putStrLn $ "Connected to gateway v" ++ show v ++ " as user " ++ show u
+run = do
+  botToken <- lookupEnv "BOT_TOKEN"
+  case botToken of 
+    Just t ->runBot (Bot t) $ do
+      with ReadyEvent $ \(Init v u _ _ _) ->
+        liftIO . putStrLn $ "Connected to gateway v" ++ show v ++ " as user " ++ show u
 
-  with MessageCreateEvent $ \msg@Message{..} -> do
-    when ("baka!" `isPrefixOf` messageContent && (not . userIsBot $ messageAuthor)) $ do
-      liftIO (replyPost messageContent) >>= reply msg
+      with MessageCreateEvent $ \msg@Message{..} -> do
+        when ("baka!" `isPrefixOf` messageContent && (not . userIsBot $ messageAuthor)) $ do
+          liftIO (replyPost messageContent) >>= reply msg
 
-    when ("greet!" `isPrefixOf` messageContent && (not . userIsBot $ messageAuthor)) $ do
-      reply msg $ replyGreet messageAuthor
-      
+        when ("greet!" `isPrefixOf` messageContent && (not . userIsBot $ messageAuthor)) $ do
+          reply msg $ replyGreet messageAuthor
+    Nothing -> liftIO . putStrLn $ "Bot token couldn't be retrieved. Check your environment variables."
